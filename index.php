1ab18039860f5e346e737fec402d19444bee1136
Anonymous <37533600+Chamath21@users.noreply.github.com>
2022-09-05 21:14:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<!-- 
+PPA Project - Amigos
+Online Ordering System Website -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Raj Pharmarcy Order System</title>
+        
+        <link rel="icon" type="image/x-icon" href="" />
+        
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">Raj Pharmarcy </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="user/register.php">Customer Sign up</a></li>
+                        <li class="nav-item"><a class="nav-link" href="user/index.php">Customer Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="admin/index.php">Admin Panel</a></li>
+
+                    </ul>
+                </div>
+          </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-heading text-uppercase">All Your Medical Needs Within Your Reach</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">What We Provide&nbsp;</a>
+            </div>
+        </header>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Services We Provide</h2>
+                    <h3 class="section-subheading text-muted">Our Pharmarcy is your trusted source of pharmaceutical services and products.We have what you are looking for...</h3>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-square fa-stack-2x text-primary"></i>
+                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Online Ordering</h4>
+                
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-square fa-stack-2x text-primary"></i>
+                            <i class="fas fa-box fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Order Tracking</h4>
+        
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-square fa-stack-2x text-primary"></i>
+                            <i class="fas fa-truck fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Fast Delivery</h4>
+                      
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+  
+        <!-- Footer-->
+        <footer class="footer py-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <a>© Raj Pharmarcies - Online Ordering System  |  @Amigos</a>
+            
+                </div>
+            </div>
+        </footer>
+        <!-- Portfolio Modals-->
+
+      
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
